d5db96ff8c3517bebaac37af4de12eb301f9910f
walsan07 <walsan679@gmail.com>
2025-03-24 23:58:47 +0530

    updated backend url to use render

--- src/components/UploadSection.tsx
+++ src/components/UploadSection.tsx
@@ -94,7 +94,7 @@ const UploadSection = ({ onDataReceived }: UploadSectionProps) => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5001/upload', {
+      const response = await fetch('https://ltcg-backend.onrender.com/upload', {
         method: 'POST',
         body: formData
       });
